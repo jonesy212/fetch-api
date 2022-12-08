208e6af15697e696fea183571c7eff7c21205efd
Marcus Jones <indelibleink84@gmail.com>
2022-12-07 18:33:54 -0600

    update api

---
+++ index.js
@@ -0,0 +1,9 @@
+let response = await fetch('https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits');
+
+// get one header
+alert(response.headers.get('Content-Type')); // application/json; charset=utf-8
+
+// iterate over all headers
+for (let [key, value] of response.headers) {
+  alert(`${key} = ${value}`);
+}
